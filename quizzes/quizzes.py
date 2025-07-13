dbfa49055ee76596e303a7514bbd86b88bcb5b7c
Gergely Gidofalvi <ggidofal4@gmail.com>
2025-07-12 17:15:39 -0700

    added more questions

--- quizzes/quizzes.py
+++ quizzes/quizzes.py
@@ -1,4 +1,4 @@
-diff = [
+num_q = [
     {
         "question": "Estimate the self‑diffusion coefficient of SPC/E water at 300 K (2 sig‑figs):",
         "type": "numeric",
@@ -11,4 +11,16 @@ diff = [
     }
 ]
 
-#QUIZZES = {"diff": diff}
+many_q = [
+    {
+        "question": "Which are valid units of energy? (select all)",
+        "type": "many_choice",
+        "answers": [
+            {"answer": "kJ mol⁻¹", "correct": True, "feedback": "Yes—kilojoules per mole."},
+            {"answer": "parsec", "correct": False, "feedback": "That is a distance."},
+            {"answer": "eV", "correct": True, "feedback": "Electron‑volts are energy."}
+        ]
+    }
+]
+
+QUIZZES = {"num_q": num_q , "many_q": many_q}
